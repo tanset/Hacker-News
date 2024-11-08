3fd160720006f52ab1542d88130c942c2cf0ab6a
tanset <albert944077@gmail.com>
2024-11-08 22:21:00 +0800

    Initial Commit

---
+++ Hacker_News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hacker_News
+//
+//  Created by 張皓宇 on 2024/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hacker_News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker_News
+//
+//  Created by 張皓宇 on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
